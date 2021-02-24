59d6507f2a59567320af2c9df3b8c51b9f655a31
Nestor <nestor.venegas95@gmail.com>
2021-02-23 21:30:26 -0600

    commit tree 72a6f053726a72b00b35b1b1243bea45887d366d

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+require "pry"
+
+class School
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+
+  def roster
+    roster = {}
+  end 
+
+  def add_student(name, grade)
+    roster[grade]? roster[grade] << name : return
+     end
+    roster
+  end 
+end
+ 
